2e225f14e526892c7cc774ed1a1d9f6686f926f4
Aysun02 <104448452+Aysun02@users.noreply.github.com>
2022-05-31 16:45:53 +0400

    Add files via upload

---
+++ Static/Static(Person).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Person
+    {
+      
+        public static int Age;
+        public void count ()
+        {
+            Age++;
+        }
+        public int GetAge()
+        {
+            return Age;
+        }
+    }
+}
