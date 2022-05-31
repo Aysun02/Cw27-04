bfd0c4f6b0b3f6594edc11768d353b83f2cc45c6
Aysun02 <104448452+Aysun02@users.noreply.github.com>
2022-05-31 16:44:37 +0400

    Add files via upload

---
+++ Static/Static.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Konul = new Person();
+           
+            Person Aysun = new Person();
+
+            Person Ebulfet = new Person();
+
+            Person Fezail = new Person();
+
+            Konul.count();
+            Console.WriteLine("Age for Konul: {0}", Konul.GetAge());
+
+            Aysun.count();
+            Console.WriteLine("Age for Aysun: {0}", Aysun.GetAge());
+
+            Ebulfet.count();
+            Console.WriteLine("Age for Ebulfet: {0} ", Ebulfet.GetAge());
+
+            Fezail.count();
+            Console.WriteLine("Age for Fezail: {0}", Fezail.GetAge());
+
+
+
+        }
+    }
+}
